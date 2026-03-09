c0a98882e39fb8e4049b4d2eacdd383de92242a8
DerpoDog <lars.puype@gmail.com>
2026-03-09 13:48:58 +0100

    ssss

--- src/auth.js
+++ src/auth.js
@@ -1,15 +1,14 @@
 const tenantId = "8c9cab84-3a29-4a80-ac5f-b144726d1431";
 const clientId = "8ca78a18-64c4-428b-9d38-dec2694411fd";
 const apiClientId = "b55a8091-76a9-47c7-8c03-7f198d75680d";
-const policy = "SignupSignin";
-
-// Use the same domain shown by your External ID / user flow setup
-const authorityBase = "https://testcustomers11.ciamlogin.com";
+const policy = "Signupsignin";
+const authorityHost = "testcustomers11.ciamlogin.com";
 
 const msalConfig = {
     auth: {
         clientId,
-        authority: `${authorityBase}/${tenantId}/${policy}`,
+        authority: `https://${authorityHost}/${tenantId}/${policy}/v2.0`,
+        knownAuthorities: [authorityHost],
         redirectUri: window.location.origin,
         postLogoutRedirectUri: window.location.origin
     },
@@ -50,9 +49,8 @@ function getAccount() {
 }
 
 async function login() {
-    const result = await msalInstance.loginPopup(loginRequest);
-    msalInstance.setActiveAccount(result.account);
-    return result;
+    await msalInstance.initialize();
+    return msalInstance.loginPopup(loginRequest);
 }
 
 // Use this instead if you want full-page redirect instead of popup:
